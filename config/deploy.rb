4f7115044817356cf700287f3ef1a2be7980e6e1
Paul Kruger <paul@miamiphp.org>
2008-10-17 00:43:41 -0400

    updating git server name

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "tutorial"
 set :domain, 'webserver'
 
 set :scm, :git
-set :repository, "git://github.com:miamiphp/mu.git"
+set :repository, "git@github.com:miamiphp/mu.git"
 
 set :deploy_via, :remote_cache
 
@@ -10,5 +10,5 @@ server "cap.speeduneed.com", :app, :web, :db, :primary => true
 set :deploy_to, "/var/www/vhosts/speeduneed.com/subdomains/cap/httpdocs/"
 ssh_options[:port] = 2221
 set :user, "pkuger"
-;set :runner, "root"
-set :use_sudo, true
+set :runner, "root"
+set :use_sudo, false
