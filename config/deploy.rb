1b67d8f93122c22d15664475695d862b06e9a82c
Paul Kruger <paul@miamiphp.org>
2008-10-16 22:46:45 -0400

    commented out runner

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,5 +10,5 @@ server "cap.speeduneed.com", :app, :web, :db, :primary => true
 set :deploy_to, "/var/www/vhosts/speeduneed.com/subdomains/cap/httpdocs/"
 ssh_options[:port] = 2221
 set :user, "pkuger"
-set :runner, "root"
+;set :runner, "root"
 
